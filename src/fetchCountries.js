e476d5a3a808af0fa0030d509b690aba5dac3943
Rdk001 <rmaslenchenko95@gmail.com>
2023-03-01 16:26:33 +0100

    1 task working)

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+// const properties = 'fields=name.official,capital,populationflags.svg,languages';
+// function fetchCountries(name) {
+//   return fetch(`https://restcountries.com/v3.1/name/${name}`).then(r => {
+//     return r
+//       .json()
+//       .then(console.log)
+//       .catch(error => console.log(error.message));
+//   });
+// }
+
+// ?${properties}
+// ============================================================
+
+// https://restcountries.com/v3.1/name/columbia
+// https://restcountries.com/v3.1/name/peru
